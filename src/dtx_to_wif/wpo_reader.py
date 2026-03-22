939caf6a1c0a4e443229c0e625f817c54c80b6a7
Russell Owen <rowen@uw.edu>
2026-03-22 14:03:47 -0700

    wpo_reader: update to use BinaryFileReader

--- src/dtx_to_wif/wpo_reader.py
+++ src/dtx_to_wif/wpo_reader.py
@@ -4,6 +4,7 @@ import enum
 import warnings
 from typing import BinaryIO
 
+from .binary_file_reader import BinaryFileReader
 from .pattern_data import PatternData, WarpWeftData
 
 # The required value of the first byte for this to be a WeavePoint file
@@ -39,14 +40,15 @@ def read_wpo(f: BinaryIO, filename: str) -> PatternData:
         f: A readable binary file
         filename: The file name. Used as the pattern name.
     """
-    id_code = read_int(f, 1)
+    bfr = BinaryFileReader(f=f, big_endian=True)
+    id_code = bfr.read_uint8()
     if id_code != WPO_ID:
         raise RuntimeError(f"ID code {id_code} != {WPO_ID}")
 
-    file_version = read_int(f, 1)
+    file_version = bfr.read_uint8()
     if file_version > 7:
         warnings.warn("File version > 7; the results may not be correct")
-    file_type_int = read_int(f, 1)  # file type: 100=dobby  101=tie-up  102=tie-up II
+    file_type_int = bfr.read_uint8()  # file type: 100=dobby  101=tie-up  102=tie-up II
     # TODO: when we require Python 3.12 or later, simplify this to:
     #     if file_type_int not in FileTypes:
     #         ...raise RuntimeError...
@@ -56,22 +58,22 @@ def read_wpo(f: BinaryIO, filename: str) -> PatternData:
         file_types_str = ", ".join(str(i) for i in FileTypes)
         raise RuntimeError(f"Unknown {file_type_int=}; must be in {file_types_str}")
     file_type = FileTypes(file_type_int)
-    read_bytes(f, 1)  # zoom: a value between 1 and 20
-    num_shafts = read_int(f, 1)
-    num_treadles = read_int(f, 1)
-    read_bytes(f, 1)  # ignore a
-    is_rising_shed = read_int(f, 1) == 0
-    read_bytes(f, 1)  # ignore b
-    epi = read_int(f, 1)  # originally "ignore c"
-    ppi = read_int(f, 1)  # originally "ignore d"
-    read_bytes(f, 2)  # ignore e-f
-    num_ends = read_int(f, 2)
-    num_picks = read_int(f, 2)
-    read_bytes(f, 7)  # ignore g-m
+    bfr.read_uint8()  # zoom: a value between 1 and 20
+    num_shafts = bfr.read_uint8()
+    num_treadles = bfr.read_uint8()
+    bfr.read_bytes(1)  # ignore a
+    is_rising_shed = bfr.read_uint8() == 0
+    bfr.read_bytes(1)  # ignore b
+    epi = bfr.read_uint8()  # originally "ignore c"
+    ppi = bfr.read_uint8()  # originally "ignore d"
+    bfr.read_bytes(2)  # ignore e-f
+    num_ends = bfr.read_uint16()
+    num_picks = bfr.read_uint16()
+    bfr.read_bytes(7)  # ignore g-m
 
-    threading = {key: set([value]) for key, value in read_dict_of_int(f).items()}
-    warp_colors = {key: value + 1 for key, value in read_dict_of_int(f).items()}
-    weft_colors = {key: value + 1 for key, value in read_dict_of_int(f).items()}
+    threading = {key: set([value]) for key, value in read_dict_of_int(bfr).items()}
+    warp_colors = {key: value + 1 for key, value in read_dict_of_int(bfr).items()}
+    weft_colors = {key: value + 1 for key, value in read_dict_of_int(bfr).items()}
     default_warp_color = warp_colors.get(len(warp_colors))
     default_weft_color = weft_colors.get(len(weft_colors))
 
@@ -79,21 +81,25 @@ def read_wpo(f: BinaryIO, filename: str) -> PatternData:
     liftplan: dict[int, set[int]] = {}
     tieup: dict[int, set[int]] = {}
     if file_type == FileTypes.LIFTPLAN:
-        liftplan = read_bitmask_sequence(f, num_shafts)
+        liftplan = read_bitmask_sequence(bfr, bits_per_mask=num_shafts)
     elif file_type == FileTypes.MULTIPLE_TREADLING:
-        tieup = read_bitmask_sequence(f, num_shafts, num_masks=num_treadles)
-        treadling = read_bitmask_sequence(f, num_treadles)
+        tieup = read_bitmask_sequence(
+            bfr, bits_per_mask=num_shafts, num_masks=num_treadles
+        )
+        treadling = read_bitmask_sequence(bfr, bits_per_mask=num_treadles)
     else:
         # single treadling
-        tieup = read_bitmask_sequence(f, num_shafts, num_masks=num_treadles)
-        treadling = {key: set([value]) for key, value in read_dict_of_int(f).items()}
+        tieup = read_bitmask_sequence(
+            bfr, bits_per_mask=num_shafts, num_masks=num_treadles
+        )
+        treadling = {key: set([value]) for key, value in read_dict_of_int(bfr).items()}
 
-    read_bytes(f, 66)  # ignore "translation grids"
+    bfr.read_bytes(66)  # ignore "translation grids"
 
-    color_table_len = read_int(f, 1)
+    color_table_len = bfr.read_uint8()
     color_range = (0, 255) if file_version >= 4 else (0, 63)
     color_table: dict[int, tuple[int, int, int]] = {
-        i + 1: read_rgb(f) for i in range(color_table_len)
+        i + 1: read_rgb(bfr) for i in range(color_table_len)
     }
 
     return PatternData(
@@ -138,7 +144,7 @@ def num_bytes_for_bits(num_bits: int) -> int:
 
 
 def read_bitmask_sequence(
-    f: BinaryIO, bits_per_mask: int, num_masks: int | None = None
+    bfr: BinaryFileReader, bits_per_mask: int, num_masks: int | None = None
 ) -> dict[int, set[int]]:
     """Read a sequence of bitmasks, e.g. tieup, liftplan, or multiple treadles.
 
@@ -163,8 +169,11 @@ def read_bitmask_sequence(
     bytes_per_mask = num_bytes_for_bits(bits_per_mask)
 
     if num_masks is None:
-        num_masks = read_int(f, 2)
-    masks = [read_int(f, bytes_per_mask, big_endian=False) for _ in range(num_masks)]
+        num_masks = bfr.read_uint16()
+    masks = [
+        bfr.read_int(num_bytes=bytes_per_mask, signed=False, big_endian=False)
+        for _ in range(num_masks)
+    ]
     return {i + 1: mask_to_int_set(mask) for i, mask in enumerate(masks)}
 
 
@@ -187,7 +196,7 @@ def read_bytes(f: BinaryIO, num_bytes: int) -> bytes:
             raise RuntimeError("Bug! read {len(data)} bytes > {num_bytes=}")
 
 
-def read_dict_of_int(f: BinaryIO) -> dict[int, int]:
+def read_dict_of_int(bfr: BinaryFileReader) -> dict[int, int]:
     """Read a sequence of one-byte integers as a dict of int
 
     The input format is:
@@ -197,20 +206,12 @@ def read_dict_of_int(f: BinaryIO) -> dict[int, int]:
 
     Return as a dict of {1-based index: integer value}
     """
-    datalen = read_int(f, 2)
-    return {i + 1: read_int(f, 1) for i in range(datalen)}
+    datalen = bfr.read_uint16()
+    return {i + 1: bfr.read_uint8() for i in range(datalen)}
 
 
-def read_int(f: BinaryIO, num_bytes: int, big_endian=True) -> int:
-    """Read num_bytes bytes as a big-endian int"""
-    data = read_bytes(f, num_bytes)
-    result = int.from_bytes(data, byteorder="big" if big_endian else "little")
-    return result
-
-
-def read_rgb(f: BinaryIO) -> tuple[int, int, int]:
+def read_rgb(bfr: BinaryFileReader) -> tuple[int, int, int]:
     """Read one set of RGB values encoded as 3 bytes."""
-    rgb_bytes = read_bytes(f, 3)
-    rgb_values = tuple(value for value in rgb_bytes)
+    rgb_values = tuple(bfr.read_uint8() for _ in range(3))
     assert len(rgb_values) == 3  # make linter happy
     return rgb_values
